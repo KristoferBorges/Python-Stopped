f251df068f7b71db9cd44c9be4f5d3ddc05c12fb
Kristofer Borges <40279965+KristoferBorges@users.noreply.github.com>
2023-04-12 11:05:08 -0700

    Delete Desafio 77.py

--- Desafio 77.py
+++
@@ -1,7 +0,0 @@
-palavras = ('Biscoito', 'Banana', 'Limao', 'Chocolate', 'Laranja', 'Churrasco', 'Bebidas')
-
-for c in palavras:
-    print(f'\nNa palavra {c.upper()} temos', end=' -> ')
-    for letra in c:
-        if letra.lower() in 'aeiou':
-            print(letra, end=' ')
